7556efc418106bacff4297eaf2723456467f9d44
Keith Leonardo <kleonardo52@gmail.com>
2021-07-02 15:11:58 +1000

    Implement login front-end

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
 import LoginPage from "./login.js";
--- src/login.js
+++ src/login.js
@@ -1,6 +1,25 @@
+import Button from "react-bootstrap/button";
+import Form from "react-bootstrap/form";
+import "./login.css";
+
 export default function LoginPage(props) {
     return (
-        <div>
+        <div className="login-form">
+            <h1>Admin Login</h1>
+            <Form>
+                <Form.Group controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="user.name@cissa.org.au"/>
+                </Form.Group>
+
+                <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password"/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+            </Form>
         </div>
     )
 }
