86339fe086b7263bad20a3a1da471bae7d65b917
George Troupis <gj.troupis@gmail.com>
2021-08-07 12:01:24 +1000

    Integrated google login with webpage editor

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
 import React from "react";
 import ReactDOM from "react";
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
-import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/pages/Home";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import LoginPage from "./login.js";
 import PostEditor from "./post_edit";
 import PostList from "./post_list";
 
 function App() {
-    return (
-        <>
-            <Router>
-                <Navbar />
-                <Switch>
-                    <Route path='/' exact component={Home} />
-                    <Route path="/login" component={LoginPage} />
-                    <Route path="/post_edit/:id" component={PostEditor} />
-                    <Route path="/post_edit" component={PostEditor} />
-                    <Route path="/post_list" component={PostList} />
-                </Switch>
-                <Footer />
-            </Router>
-        </>
-    );
-    
-}
+  function isLoggedIn() {
+    const googleToken = localStorage.getItem("googleToken");
 
+    if (googleToken) {
+      return true;
+    }
+    alert("not logged in");
+    return false;
+  }
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route
+            path="/post_edit/:id"
+            render={() => (isLoggedIn() ? <PostEditor /> : <Redirect to="/" />)}
+          />
+          <Route
+            path="/post_edit"
+            render={() => (isLoggedIn() ? <PostEditor /> : <Redirect to="/" />)}
+          />
+          <Route
+            path="/post_list"
+            render={() => (isLoggedIn() ? <PostList /> : <Redirect to="/" />)}
+          />
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  );
+}
 
 // ReactDOM.render(
 //   <React.StrictMode>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,91 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import "./Navbar.css"
-import { Button } from './Button';
-import logo from './images/logo.png'; // Tell webpack this JS file uses this image
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button";
+import logo from "./images/logo.png"; // Tell webpack this JS file uses this image
+import GoogleLogin from "react-google-login";
+import axios from "axios";
+
 console.log(logo);
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+  window.addEventListener("resize", showButton);
 
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
-    window.addEventListener('resize', showButton);
+  const handleLogin = async (googleData) => {
+    const res = await axios.post(
+      "/api/auth/google",
+      {
+        token: googleData.tokenId,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    // store returned user somehow
+    if (res.status !== 200) {
+      console.log("error");
+    }
 
-    return (
-        <div>
-            <nav className="header navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        <img className="navbar-logo-img" src={logo} alt="Logo" />
-                    </Link>
-                    <div className='menu-icon' onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
-                    </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        {/* <li className='nav-item'>
+    console.log("logged in successfully");
+    localStorage.setItem("googleToken", googleData.tokenId);
+    <Redirect to="/post_list" />;
+  };
+
+  return (
+    <div>
+      <nav className="header navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            <img className="navbar-logo-img" src={logo} alt="Logo" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            {/* <li className='nav-item'>
                             <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                                 Join CISSA
                             </Link>
                         </li> */}
-                        <li className='nav-item'>
-                            <Link to="post_edit" className="nav-links" onClick={closeMobileMenu}>
-                                Create Post
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to="/login" className="nav-links" onClick={closeMobileMenu}>
-                                Login
-                            </Link>
-                        </li>
-                    </ul>
-                    {/* {button && <Button buttonStyle='btn--primary'>Login</Button>} */}
-                </div>
-            </nav>
+            <li className="nav-item">
+              <Link
+                to="post_edit"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Create Post
+              </Link>
+            </li>
+            <li className="nav-item">
+              <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                buttonText="Log in with Google"
+                onSuccess={handleLogin}
+                onFailure={handleLogin}
+                cookiePolicy={"single_host_origin"}
+              />
+            </li>
+          </ul>
+          {/* {button && <Button buttonStyle='btn--primary'>Login</Button>} */}
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
-
+export default Navbar;
